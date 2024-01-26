dae8eced9fdb5f680cb62cb6ed16f98d0baa4db5
Hansajaa <nilanahansaja21@gmail.com>
2024-01-26 14:49:17 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import CoverPhoto from "./CoverPhoto";
+import Footer from "./footer";
+import Navbar from "./navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+        <Navbar />
+        <CoverPhoto/>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
